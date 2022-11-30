259e61ce453b58e8bef40f89c15be2cbb6b23266
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-11-30 09:45:25 -0600

    commit tree 19e6a048a1e254ef8dc6dee91f434887389b8f2c

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+
+    def initialize(brand = "Adidas", color = "red", size = 9.5, material = "suede", condition= "tattered")
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
+
+shoe = Shoe.new("Nike")
+shoe.cobble
